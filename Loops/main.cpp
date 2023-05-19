c60b6c14f76df232bf8385adb38e4b7df06ed1dd
Rhys2000 <rhys2kjj@gmail.com>
2023-05-19 09:09:43 -0700

    Completed Errors Lesson

--- Loops/main.cpp
+++ Loops/main.cpp
@@ -144,4 +144,146 @@ int main() {
         }
     }
  
+    /*
+    - The story goes that on September 9th, 1947, computer scientist Grace Hopper found a moth in the Harvard Mark II computer’s log book and reported the world’s first literal computer bug. However, the term “bug”, in the sense of technical error, dates back at least to 1878 and with Thomas Edison.
+    - On your programming journey, you are destined to encounter innumerable red errors. Some even say, that debugging is over 75% of the development time. But what makes a programmer successful isn’t avoiding errors, it’s knowing how to find the solution.
+    - In C++, there are many different ways of classifying errors, but they can be boiled down to four categories:
+        > Compile-time errors: Errors found by the compiler.
+        > Link-time errors: Errors found by the linker when it is trying to combine object files into an executable program.
+        > Run-time errors: Errors found by checks in a running program.
+        > Logic errors: Errors found by the programmer looking for the causes of erroneous results.
+    - In this mini-lesson, we will be looking at different errors and different error messages, and we’ll teach you how to think about errors in your code a little differently.
+    */
+    
+    /*
+    - When we are writing C++ programs, the compiler is our first line of defense against errors.
+    - There are two types of compile-time errors:
+        > Syntax errors: Errors that occur when we violate the rules of C++ syntax.
+        > Type errors: Errors that occur when there are mismatch between the types we declared.
+    - Some common syntax errors are:
+        > Missing semicolon ;
+        > Missing closing parenthesis ), square bracket ], or curly brace }
+    - Some common type errors are:
+        > Forgetting to declare a variable
+        > Storing a value into the wrong type
+    - The compiler will tell us where (line number) it got into trouble and its best guess as to what is wrong.
+    */
+    
+    // 1. Try compiling the code and find the compile-time error in millionaire.cpp.
+    
+    char answer;
+    int score = 0;
+      
+    std::cout << "Who Wants To Be a Millionaire\n\n";
+    std::cout << "Question 1)\n\n";
+    std::cout << "For ordering his favorite beverages on demand, LBJ had four buttons installed in the Oval Office labeled 'Coffee', 'Tea', 'Coke', and what?\n\n";
+    std::cout << "A. Fresca   B. V8  \n";
+    std::cout << "C. Yoo-hoo  D. A&W \n\n";
+    std::cout << "Enter your answer: ";
+    std::cin >> answer;
+      
+    if (answer == 'A' || answer == 'a') {
+        score = score + 100;
+        std::cout << "Correct!\n";
+    }
+    
+    /*
+    - Sometimes the code compiles fine, but there is still a message because the program needs some function or library that it can’t find. This is known as a link-time error.
+    - As our program gets bigger, it is good practice to divide the program into separate files. After compiling them, the linker takes those separate object files and combines them into a single executable file. Link-time errors are found by the linker when it is trying to combine object files into an executable file.
+    - Some common link-time errors:
+        > Using a function that was never defined (more on this later)
+        > Writing Main() instead of main()
+    - These errors are more hard to find, but remember, Google is your friend! Here, a good Google search would be: “C++ undefined reference to main”.
+    */
+    
+    // 1. Try compiling to find the link-time error in sum.cpp.
+    
+    int num = 0;
+    int sum = 0;
+      
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+      
+    for(int i = 1; i <= num; i++) {
+        sum = sum + i;
+        std::cout << i << " ";
+    }
+      
+    std::cout << "Sum: " << sum << "\n";
+    
+    /*
+    - If our program has no compile-time errors and no link-time errors, it’ll run. This is where the fun really starts.
+    - Errors which happen during program execution (run-time) after successful compilation are called run-time errors. Run-time errors occur when a program with no compile-time errors and link-time errors asks the computer to do something that the computer is unable to reliably do.
+    - It happens after we give the ./ execute command: ./a.out
+    - Some common run-time errors:
+        > Division by zero also known as division error. These types of error are hard to find as the compiler doesn’t point to the line at which the error occurs.
+        > Trying to open a file that doesn’t exist
+    - There is no way for the compiler to know about these kinds of errors when the program is compiled.
+    */
+    
+    // 1. There’s a run-time error in ratio.cpp. This program is supposed to find the ratio of a table’s dimensions, which is supposed to be 20 x 30.
+    
+    int width = 20;
+    int length = 30;
+    int ratio = width / length;
+      
+    std::cout << ratio << "\n";
+    
+    /*
+    - Once we have removed the compile-time errors, link-time errors, and run-time errors, the program runs successfully. But sometimes, the program doesn’t do what we want it to do or no output is produced. Hmmm…
+    - These types of errors which provide incorrect output, but appears to be error-free, are called logical errors. These are one of the most common errors that happen to beginners and also usually the most difficult to find and eliminate.
+    - Logical errors solely depend on the logical thinking of the programmer. Your job now is to figure out why the program didn’t do what you wanted it to do.
+    - Some common logic errors:
+        > Program logic is flawed
+        > Some “silly” mistake in an if statement or a for/while loop
+    - Note: Logic errors don’t have error messages. Sometimes, programmers use a process called test-driven development (TDD), a way to give logic errors error messages and save yourself a lot of headaches!
+    */
+    
+    // 1. Compile and execute steps.cpp. There should be no error messages. But the program is supposed to output steps from #1 - #10. The program is somehow starting from 0… Find the logic error.
+    
+    int steps = 10;
+      
+    for(int i = 1; i <= steps; i++) {
+        std::cout << "Step #";
+        std::cout << i << "\n";
+    }
+    
+    /*
+    - Finding bugs is a huge part of a programmer’s life. Don’t be intimidated by them… embrace them. Errors in your code mean you’re trying to do something cool!
+    - In this lesson, we have learned about the four types of C++ errors:
+        > Compile-time errors: Errors found by the compiler.
+        > Link-time errors: Errors found by the linker when it is trying to combine object files into an executable program.
+        > Run-time errors: Errors found by checks in a running program.
+        > Logic errors: Errors found by the programmer looking for the causes of erroneous results.
+    - Remember, Google and Stack Overflow are a programmer’s best friends. For some more motivation, check out this blog post: Thinking About Errors in Your Code Differently. We wish you the best of luck in your bug-squashing journey.
+    */
+    
+    // 1. Optional: Because your friend didn’t debug along the way, they ended up with a super buggy program. Can you be a bug detective and help them debug their program? The final program should print a fortune message like: 🥠 Flattery will go far tonight. Final tip: If you encounter a long error message, always scroll to the top and try to debug the very first bug.
+    
+    srand(time(NULL));
+    int fortune = rand() % 10;
+      
+    if(fortune == 0) {
+        std::cout << "Flattery will go far tonight.\n";
+    } else if(fortune == 1) {
+        std::cout << "Don't behave with cold manners.\n";
+    } else if (fortune == 2) {
+        std::cout << "May you someday be carbon neutral\n";
+    } else if (fortune == 3) {
+        std::cout << "You have rice in your teeth.\n";
+    } else if (fortune == 4) {
+        std::cout << "A conclusion is simply the place where you got tired of thinking.\n";
+    } else if (fortune == 5) {
+        std::cout << "No snowflake feels responsible in an avalanche.\n";
+    } else if (fortune == 6) {
+        std::cout << "He who laughs last is laughing at you.\n";
+    } else if (fortune == 7) {
+        std::cout << "If you look back, you'll soon be going that way.\n";
+    } else if (fortune == 8) {
+        std::cout << "You will die alone and poorly dressed.\n";
+    } else if (fortune == 9) {
+        std::cout << "The fortune you seek is in another cookie.\n";
+    } else {
+        std::cout << "Hi.\n";
+    }
 }
